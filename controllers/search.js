7daf74fbf156da61063b34ba0d67cd8efd568da1
Ngân Khánh Nguyễn Thị <ngankhanh98@gmail.com>
2020-03-15 17:42:58 +0700

    hàm filter có lỗi

--- controllers/search.js
+++ controllers/search.js
@@ -1,9 +1,38 @@
-const search = require('../models/search');
+const search = require("../models/search");
 
 exports.home = async (req, res) => {
-    kynang = await search.getKyNang();
-    res.render('search', {
-        title: 'Search',
-        kynang
-    });
-}
+  kynang = await search.getKyNang();
+  res.render("search", {
+    title: "Search",
+    kynang
+  });
+};
+
+exports.filter = async (req, res, next) => {
+  var obj = req.body["KyNang"];
+  obj = obj.split(" / ");
+ 
+  var arr = [];
+  var e = new Object();
+  
+  for (let index = 0; index < obj.length; index++) {
+    const element = obj[index];
+    if(index%2==0)
+    {
+      e.ten = element;
+    }
+    else 
+    {
+      if(element.includes("Nghiệp dư")) e.trinhdo = 1;
+      else if(element.includes("Có kinh nghiệm")) e.trinhdo=2;
+      else e.trinhdo=3;
+    
+      arr.push(e);
+    }   
+  }
+
+
+  congviec = await search.filterCongViec(arr);
+  // console.log(congviec);
+  res.render("index", { title: arr });
+};
--- models/search.js
+++ models/search.js
@@ -1,8 +1,26 @@
-const {
-    db
-} = require('../config/dbs');
+const { db } = require("../config/dbs");
+var _ = require("underscore");
 
 module.exports.getKyNang = async () => {
-    const data = await db.get('kynang').value();
-    return data;
-}
+  const data = await db.get("kynang").value();
+  return data;
+};
+module.exports.getCongViec = async () => {
+  const data = await db.get("congviec").value();
+  return data;
+};
+
+module.exports.filterCongViec = async filter => {
+  const data = await db.get("congviec").value();
+  // console.log(data[0].kyNangCan[0].ten);
+
+  // console.log(filter);
+  var result = data.filter(data => {
+    console.log(data.kyNangCan);
+    console.log(filter);
+    if (data.kyNangCan == filter) {
+      console.log(true);
+      return data;
+    }
+  });
+};
