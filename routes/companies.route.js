643ecf79a5ad9acba246f4a7c62ce53445a618e1
UnknowEntity <theplayer357@gmail.com>
2020-03-16 20:41:04 +0700

    finish post jobs

--- routes/companies.route.js
+++ routes/companies.route.js
@@ -25,20 +25,31 @@ router.get("/:id/post", async (req, res, next) => {
   res.render("vwJobs/form", {
     layout: "boostrapLayout",
     title: `${company.name} post`,
+    id,
     extra:
-      '<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">',
+      '<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">' +
+      '<link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />' +
+      '<link href="/stylesheets/skilloption.css" rel="stylesheet" />',
     script:
       '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>' +
       '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>' +
-      '<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>'
+      '<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>' +
+      '<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>'
   });
 });
 
 router.post("/:id/post", async (req, res, next) => {
   const id = +req.params.id;
+  const entity = { id };
+  let company = await companyModel.get(entity);
+  req.body["company"] = company.name;
   req.body["id"] = shortid.generate();
-  req.body["company"] = companyModel.get({ id }).name;
-  jobModel.add(req.body);
+  try {
+    await jobModel.add(req.body);
+    res.status(200).send({ message: "ok" });
+  } catch (error) {
+    res.status(400).send({ message: "not ok" });
+  }
 });
 
 module.exports = router;
