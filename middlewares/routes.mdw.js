d9c512b2cf46e6d389f70496287dded4c1c2a3f1
Ngân Khánh <ngankhanh98@gmail.com>
2020-03-17 09:37:47 +0700

    Update routes.mdw.js

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -2,8 +2,10 @@ module.exports = app => {
   var indexRouter = require("../routes/index");
   var jobsRouter = require("../routes/jobs.route");
   var companysRouter = require("../routes/companies.route");
-
+  var searchRouter = require("../routes/search");
+  
   app.use("/", indexRouter);
   app.use("/jobs", jobsRouter);
   app.use("/companies", companysRouter);
+  app.use("/search", searchRouter);
 };
