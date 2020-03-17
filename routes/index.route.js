bdc1ab971591ee12716b853f5a827b48531f5c92
Dien Nguyen <62097337+dien-hn@users.noreply.github.com>
2020-03-17 10:02:27 +0700

    redirect traffic from \/` to `/jobs`

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.redirect('http://localhost:3000/jobs');
 });
 
 module.exports = router;
