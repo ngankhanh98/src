47d9f82cf9128d25126a494cf1d19935014958ab
Ngân Khánh Nguyễn Thị <ngankhanh98@gmail.com>
2020-03-17 09:33:40 +0700

    router /search + fix dead link

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -3,9 +3,11 @@ module.exports = app => {
   var usersRouter = require("../routes/users");
   var jobsRouter = require("../routes/jobs.route");
   var companysRouter = require("../routes/companies.route");
+  var searchRouter = require("../routes/search");
 
   app.use("/", indexRouter);
   app.use("/users", usersRouter);
   app.use("/jobs", jobsRouter);
   app.use("/companies", companysRouter);
+  app.use("/search", searchRouter);
 };
