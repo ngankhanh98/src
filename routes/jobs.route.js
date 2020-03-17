2fb79de1b2ae1ee25886afbbfd4cc5fa554f1586
UnknowEntity <theplayer357@gmail.com>
2020-03-17 10:57:08 +0700

    fix bug in jobs route

--- routes/jobs.route.js
+++ routes/jobs.route.js
@@ -1,31 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const jobModel = require('../models/job.model');
+const jobModel = require("../models/job.model");
 
 /* GET list of jobs. */
 router.get("/", async (req, res, next) => {
   const jobs = await jobModel.all();
 
-  for (let index = 0; index < jobs.length; index++) {
-    for (let index2 = 0; index2 < jobs[index].skills.length; index2++) {
-      let temp = jobs[index].skills[index2].level;
-      let text = "None";
-      switch (temp) {
-        case 1:
-          text = "Nghiệp dư";
-          break;
-        case 2:
-          text = "Có kinh ngiệm";
-          break;
-        case 3:
-          text = "Chuyên nghiệp";
-        default:
-          break;
+  if (!isNaN(jobs[0].skills[0].level)) {
+    for (let index = 0; index < jobs.length; index++) {
+      for (let index2 = 0; index2 < jobs[index].skills.length; index2++) {
+        let temp = jobs[index].skills[index2].level;
+        let text = "None";
+        switch (temp) {
+          case 1:
+            text = "Nghiệp dư";
+            break;
+          case 2:
+            text = "Có kinh ngiệm";
+            break;
+          case 3:
+            text = "Chuyên nghiệp";
+          default:
+            break;
+        }
+        jobs[index].skills[index2].level = text;
       }
-      jobs[index].skills[index2].level = text;
     }
   }
+
   res.render("vwJobs/index", {
     layout: "bootstrapLayout",
     jobs: jobs,
@@ -34,11 +37,11 @@ router.get("/", async (req, res, next) => {
 });
 
 /* GET job detail. */
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   const entity = { id: +req.params.id };
   const row = jobModel.get(entity);
 
-  res.render('vwJobs/detail', {
+  res.render("vwJobs/detail", {
     job: row,
     empty: row === undefined
   });
