c33e9ff6014768c8a3fefd684017899cb78fcb27
Dien Nguyen <62097337+dien-hn@users.noreply.github.com>
2020-03-17 10:28:07 +0700

    minor update for `views/vWjobs/index.hbs`

--- routes/jobs.route.js
+++ routes/jobs.route.js
@@ -4,12 +4,32 @@ var router = express.Router();
 const jobModel = require('../models/job.model');
 
 /* GET list of jobs. */
-router.get('/', async (req, res, next) => {
-  const rows = await jobModel.all();
+router.get("/", async (req, res, next) => {
+  const jobs = await jobModel.all();
 
-  res.render('vwJobs/index', {
-    jobs: rows,
-    empty: rows.length == 0
+  for (let index = 0; index < jobs.length; index++) {
+    for (let index2 = 0; index2 < jobs[index].skills.length; index2++) {
+      let temp = jobs[index].skills[index2].level;
+      let text = "None";
+      switch (temp) {
+        case 1:
+          text = "Nghiệp dư";
+          break;
+        case 2:
+          text = "Có kinh ngiệm";
+          break;
+        case 3:
+          text = "Chuyên nghiệp";
+        default:
+          break;
+      }
+      jobs[index].skills[index2].level = text;
+    }
+  }
+  res.render("vwJobs/index", {
+    layout: "bootstrapLayout",
+    jobs: jobs,
+    extra: '<link href="/stylesheets/jobIndex.css" rel="stylesheet" />'
   });
 });
 
