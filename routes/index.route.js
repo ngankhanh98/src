fe2b8e0e12293184d7006f430b2123769802505f
Ngân Khánh <ngankhanh98@gmail.com>
2020-03-19 18:02:57 +0700

    Update index.route.js

--- routes/index.route.js
+++ routes/index.route.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.redirect('https://csc13116-src.herokuapp.com/');
+  res.redirect('https://csc13116-src.herokuapp.com/jobs');
 });
 
 module.exports = router;
